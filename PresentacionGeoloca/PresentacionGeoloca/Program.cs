2b8f39b0b2a19d7a4172878078d72f43296e0220
prado <pradojimenezmaria@gmail.com>
2023-11-12 13:23:01 -0600

    Se agregaron los comandos para Ejecutar Formularios

--- PresentacionGeoloca/PresentacionGeoloca/Program.cs
+++ PresentacionGeoloca/PresentacionGeoloca/Program.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace PresentacionGeoloca
 {
     public class Program
     {
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new FrmTienda());
+        }
     }
 }
