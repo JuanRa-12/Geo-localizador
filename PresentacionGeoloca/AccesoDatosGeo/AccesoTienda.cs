502faeec64d15e2682a213fbfa7356771944f07e
prado <pradojimenezmaria@gmail.com>
2023-11-12 12:27:56 -0600

    Se implemento IEntidades en AccesoTienda

--- PresentacionGeoloca/AccesoDatosGeo/AccesoTienda.cs
+++ PresentacionGeoloca/AccesoDatosGeo/AccesoTienda.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EntidadesGeo;
 
 namespace AccesoDatosGeo
 {
-    public class AccesoTienda
+    public class AccesoTienda : IEntidades
     {
+        public void Borrar(dynamic Entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Guardar(dynamic Entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataSet Mostrar(string filtro)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
