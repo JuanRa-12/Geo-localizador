a9ac9bedb9f6340e162fd6e339b5450fee1c76f9
prado <pradojimenezmaria@gmail.com>
2023-11-12 12:25:52 -0600

    Se agrego Interfaz de entidades a AccesoDatosGeo

---
+++ PresentacionGeoloca/AccesoDatosGeo/IEntidades.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntidadesGeo;
+
+namespace AccesoDatosGeo
+{
+    public interface IEntidades
+    {
+        void Guardar(dynamic Entidad);
+        void Borrar(dynamic Entidad);
+        DataSet Mostrar(string filtro);
+    }
+}
