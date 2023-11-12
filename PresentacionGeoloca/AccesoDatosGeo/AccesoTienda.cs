a7873c0f097eeca10cd3daa27225aece5cb2a90d
prado <pradojimenezmaria@gmail.com>
2023-11-12 12:56:37 -0600

    Se agrego Conexion a BD

--- PresentacionGeoloca/AccesoDatosGeo/AccesoTienda.cs
+++ PresentacionGeoloca/AccesoDatosGeo/AccesoTienda.cs
@@ -1,28 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EntidadesGeo;
+using MySql.Data.MySqlClient;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using ConectarBd;
 
 namespace AccesoDatosGeo
 {
     public class AccesoTienda : IEntidades
     {
+        Base b = new Base("Localhost", "root", "", "geo", 3306);
         public void Borrar(dynamic Entidad)
         {
-            throw new NotImplementedException();
+            //b.comando(String.Format("call deletePais({0})",
+            //    Entidad.Ncorrelativo));
         }
 
         public void Guardar(dynamic Entidad)
         {
-            throw new NotImplementedException();
+            b.comando(string.Format("call InsertarTienda('{0}','{1}'", Entidad.Nombre, Entidad.Direccion));
         }
 
         public DataSet Mostrar(string filtro)
         {
-            throw new NotImplementedException();
+            return b.Obtener
+                (string.Format("call ConsultarTiendas('%{0}%')",
+                filtro), "tienda");
         }
     }
 }
